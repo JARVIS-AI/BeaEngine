93058bbdd7aef171045973a47d3a0e9301bc803d
igor.gutnik <igor.gutnik@e67b0f8a-f93e-11de-ba51-6b4196a2b81e>
2010-01-28 14:23:44 +0000

    starting to implement regression tests
    git-svn-id: http://beaengine.googlecode.com/svn/trunk@74 e67b0f8a-f93e-11de-ba51-6b4196a2b81e

---
+++ unittest/gen_code.py
@@ -0,0 +1,29 @@
+import string
+import re;
+p = re.compile('\s+');
+
+f = open ("table.dasm")
+k = 0;
+for line in f.readlines ():
+    inp = p.sub (' ', string.strip (line));
+    k = k + 1;
+    tokens = string.split (inp);
+    opcode = tokens [0];
+    txt = "";
+    for t in tokens [1:]:
+        txt = txt + t + " "
+    n = len (opcode) / 2;
+    ccode = "{";
+    for i in range (0, n):
+        a = opcode [2*i]
+        b = opcode [(2*i)  +1]
+        ccode = ccode + "0x" + a + b;
+        if i < n - 1:
+            ccode = ccode + ",";
+    ccode = ccode + "};";
+    xx = "unsigned char bytes_" + str (k) + " [] = " + ccode;
+    yy = "bytes_" + str (k);
+    print xx
+    zz = "DASM (" + yy + ", \"" +string.strip (txt) + "\");";
+    print zz;
+    
