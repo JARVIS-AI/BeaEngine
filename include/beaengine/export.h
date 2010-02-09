2c6fbb05adff7760e118cbc0c6d3af7bd4bdcd8d
igor.gutnik <igor.gutnik@e67b0f8a-f93e-11de-ba51-6b4196a2b81e>
2010-02-09 08:30:22 +0000

    regressions cont.
    git-svn-id: http://beaengine.googlecode.com/svn/trunk@84 e67b0f8a-f93e-11de-ba51-6b4196a2b81e

--- include/beaengine/export.h
+++ include/beaengine/export.h
@@ -49,7 +49,7 @@
 #  endif
 # elif defined(_WIN32) || defined(_WIN64)
 #  ifdef __BORLANDC__
-#    define bea__api_export__ 
+#    define bea__api_export__   __declspec(dllexport)
 #    define bea__api_import__ 	__declspec(dllimport)
 #  elif defined(__WATCOMC__)
 #    define bea__api_export__	__declspec(dllexport)
